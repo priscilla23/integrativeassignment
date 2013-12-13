dfaceb26186450d0f46ad44e5b7024b117e0ebee
priscilla23 <cnnp0113@humbermail.ca>
2013-12-13 13:03:43 -0500

    finished assignment

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Not Far From the Tree</title>
+
+		<link rel="stylesheet" href="styles/reset.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="styles/css/main.css" type="text/css" media="screen" />
+	</head>
+	<body>
+		<header>
+			<div class="content">
+				<a href="index.html"><img src="images/logo.png" alt="nfft logo" id="logo"></a>
+				<div id="nav">
+				<div id="main-nav" class="center">
+					<ul>
+						<li><a href="#cta" id="nav_section1" class="active">Home</a></li>
+						<li><a href="#about" id="nav_section2">About</a></li>
+						<li><a href="#gallery" id="nav_section3">Gallery</a></li>
+						<li><a href="#takepart" id="nav_section5">Contact</a></li>
+					</ul>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<div name="cta" id="cta" class="section">
+				<h1>Not Far from the Tree</h1>
+						<h2>We put Toronto's food to good use, by picking and sharing with the community!</h2>
+								<button type="button" id="donate">Donate</button>
+								<button type="button" id="register">Register Your Tree</button>
+		</div>
+
+		<div name="about" id="about" class="section">
+			<h2>About Us</h2>
+				<div class="content">
+						<img src="images/aboutimg.png" alt="girl picking fruit from tree">
+
+						<p>Not Far From The Tree puts Toronto’s fruit to good use by picking and sharing the bounty</p>
+
+						<p>When a homeowner can’t keep up with the abundant harvest produced by their tree, they let us know and we mobilize our volunteers to pick the bounty. The harvest is split three ways: 1/3 is offered to the tree owner, 1/3 is shared among the volunteers, and 1/3 is delivered by bicycle to be donated to food banks, shelters, and community kitchens in the neighbourhood so that we’re putting this existing source of fresh fruit to good use. It’s a win-win-win situation!</p>
+
+						<p>This simple act has profound impact. With an incredible crew of volunteers, we’re making good use of healthy food, addressing climate change with hands-on community action, and building community by sharing the urban abundance.</p>
+
+						<p>As a project of Tides Canada Initiatives Society, Not Far From The Tree is committed to the principles of dignity, independence, integration and equal opportunity. We believe that helping to create a country, and an organization, where every person can fully participate benefits our people, our partners and our communities.</p>
+
+						<p>Tides Canada’s Accessibility Policy and Plan outline the various considerations our staff, volunteers and contractors need to take into account when communicating with an individual with a disability. They also outline our guidelines regarding training, what to do in the case of a temporary disruption and how we accept feedback regarding the way Tides Canada has interacted with a person with a disability.</p>
+				</div>
+		</div>
+
+		<div id="achievements">
+			<h2>Achievements</h2>
+				<div class="content">
+					<div id="box1" class="ach">
+						<p class="small">Pounds picked in 2013</p>
+						<p class="total">22,154</p>
+						<p class="small">from 348 trees</p>
+					</div>
+
+					<div id="box2" class="ach">
+						<p class="small">Pounds picked in 2012</p>
+						<p class="total">12,512</p>
+						<p class="small">from 243 trees</p>
+					</div>
+
+					<div id="box3" class="ach">
+						<p class="small">Pounds picked in 2011</p>
+						<p class="total">9,196</p>
+						<p class="small">from 141 trees</p>
+					</div>
+
+					<div id="box4" class="ach">
+						<p class="small">Pounds picked in 2010</p>
+						<p class="total">16,695</p>
+						<p class="small">from 228 trees</p>
+					</div>
+
+					<div id="box5" class="ach">
+						<p class="small">Pounds picked in 2009</p>
+						<p class="total">8,135</p>
+						<p class="small">from 184 trees</p>
+					</div>
+
+					<div id="box6" class="ach">
+						<p class="small">Pounds picked in 2008</p>
+						<p class="total">3,003</p>
+						<p class="small">from 40 trees</p>
+					</div>
+				</div>
+		</div>
+
+		<div name="gallery" id="gallery" class="section">
+			<h2>Sneak Peak at our Gallery</h2>
+				<div class="content">
+
+					<a href="#">
+					<img src="images/gall1.png" alt="omg"/>
+					</a>
+
+					<a href="#">
+					<img src="images/gall2.png" alt="img"/>
+					</a>
+
+					<a href="#">
+					<img src="images/gall3.png" alt="img"/>
+					</a>
+
+					<a href="#">
+						<img src="images/gall4.png" alt="img"/>
+					</a>
+
+					<a href="#">
+						<img src="images/gall5.png" alt="img"/>
+					</a>
+
+					<a href="#">
+						<img src="images/gall6.png" alt="img"/>
+					</a>
+
+					<a href="#">
+						<img src="images/gall7.png" alt="img"/>
+					</a>
+
+					<a href="#">
+						<img src="images/gall8.png" alt="img"/>
+					</a>
+
+					<a href="">
+						<img src="images/gall9.png" alt="img"/>
+					</a>
+			
+					<a href="#">
+						<img src="images/gall10.png" alt="img" id="lastimg"/>
+					</a>
+
+					<a href="http://www.flickr.com/photos/notfarfromthetree/" id="flickrlink">View Ablum on Flickr!</a>
+				</div>
+		</div>
+
+		<div name="takepart" id="takepart" class="section">
+			<h2>Contact us!</h2>
+	
+
+			<form id="myform" action="welcome.php" method="post">
+
+			 Name:
+            	<input type="text" id="lastName"
+                name="lastName" size="29" maxlength="30" />
+                <br />
+                Email:
+                <input type="text" id="email"
+                name="firstName" size="30" maxlength="30" />
+                <br />
+                <p>Comments:</p><textarea rows="8" cols="30"></textarea><br/>
+                <input type="submit"  id="submitb" value="Submit!" />
+           
+      		</form>
+		</div>
+
+				<div id="socialBar">
+					<h3> Connect Socially with Not Far From the Tree</h3>
+						<ul>
+							<li><a href=""><img src="images/twitter.png" alt="twittericon"/></a></li>
+							<li><a href=""><img src="images/facebook.png" alt="facebookicon"/></a></li>
+							<li><a href=""><img src="images/rss.png" alt="rssicon"/></a></li>
+							<li><a href=""><img src="images/linkedin.png" alt="linkedinicon"/></a></li>
+						</ul>
+				</div>
+
+				<footer>
+					<ul>
+						<li><a href="#">help</a></li>
+						<li><a href="#">volunteer</a></li>
+						<li><a href="#">jobs</a></li>
+						<li><a href="#">legal</a></li>
+						<li><a href="#">privacy</a></li>
+						<li><a href="#">terms of use</a></li>
+					</ul>
+						<p>Not Far From the Tree &#169 20213 All Rights Reserved</p>
+						<!--add sponsor -->
+				</footer>
+
+		</body>
+</html>
